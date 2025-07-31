b602f913fee0315d561cea1961f6e1929a938980
Aditi Vaidya <aditi.vaidya@konverge.ai>
2025-07-31 13:26:13 +0530

    Committing 18 items from workspace 3311b742-3f58-4090-bb92-97cbf996f60f

---
+++ Workspace/Notebook/Load Sales.Notebook/notebook-content.py
@@ -0,0 +1,58 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "2d52b5bd-5e31-467f-8e38-178b4f37d61e",
+# META       "default_lakehouse_name": "Lab5",
+# META       "default_lakehouse_workspace_id": "3311b742-3f58-4090-bb92-97cbf996f60f",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "2d52b5bd-5e31-467f-8e38-178b4f37d61e"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# PARAMETERS CELL ********************
+
+table_name = "sales"
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from pyspark.sql.functions import *
+
+# Read the new sales data
+df = spark.read.format("csv").option("header","true").load("Files/new_data/*.csv")
+
+## Add month and year columns
+df = df.withColumn("Year", year(col("OrderDate"))).withColumn("Month", month(col("OrderDate")))
+
+# Derive FirstName and LastName columns
+df = df.withColumn("FirstName", split(col("CustomerName"), " ").getItem(0)).withColumn("LastName", split(col("CustomerName"), " ").getItem(1))
+
+# Filter and reorder columns
+df = df["SalesOrderNumber", "SalesOrderLineNumber", "OrderDate", "Year", "Month", "FirstName", "LastName", "EmailAddress", "Item", "Quantity", "UnitPrice", "TaxAmount"]
+
+# Load the data into a table
+df.write.format("delta").mode("append").saveAsTable(table_name)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
